f9d247404db1c941d8dcd57e3b173780df920873
viggy815 <viggy815@aol.com>
2017-08-07 17:20:05 -0400

    updates
    deleted 2 inactive labels

--- VigneroRectangleExceptions/VigneroRectangleExceptions/RectangleAreaCalc.Designer.cs
+++ VigneroRectangleExceptions/VigneroRectangleExceptions/RectangleAreaCalc.Designer.cs
@@ -50,8 +50,6 @@
             this.btnCalculate = new System.Windows.Forms.Button();
             this.btnClear = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.gbMeasurements.SuspendLayout();
             this.dbArea.SuspendLayout();
             this.gbPerimeter.SuspendLayout();
@@ -285,31 +283,11 @@
             this.btnExit.UseVisualStyleBackColor = true;
             this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(176, 190);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 11;
-            this.label1.Text = "label1";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(321, 190);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 12;
-            this.label2.Text = "label2";
-            // 
             // RectangleAreaCalc
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(669, 353);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnClear);
             this.Controls.Add(this.btnCalculate);
@@ -361,8 +339,6 @@
         private System.Windows.Forms.Button btnExit;
         private System.Windows.Forms.Label lblArea;
         private System.Windows.Forms.Label lblPerimeter;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
     }
 }
 
